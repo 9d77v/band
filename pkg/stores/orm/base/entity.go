5c22e6f0fd960cb1b07e6f5b35eda790405c5be7
9d77v <9d77v@9d77v.me>
2024-04-18 16:55:49 +0800

    feat: add int64 model

--- pkg/stores/orm/base/entity.go
+++ pkg/stores/orm/base/entity.go
@@ -13,3 +13,10 @@ type Entity struct {
 	UpdatedAt time.Time      `gorm:"comment:更新时间"`
 	DeletedAt gorm.DeletedAt `gorm:"index;comment:删除时间"`
 }
+
+type Model struct {
+	ID        int64          `gorm:"primaryKey;comment:主键"`
+	CreatedAt time.Time      `gorm:"comment:创建时间"`
+	UpdatedAt time.Time      `gorm:"comment:更新时间"`
+	DeletedAt gorm.DeletedAt `gorm:"index;comment:删除时间"`
+}
